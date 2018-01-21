ca5a2cfb65fb45f4f0f9058dd37c62c52533f1ff
Alexandre Fernandez <alexandre.fernandez@etu.u-pec.fr>
2018-01-21 16:34:56 +0100

    beq_instr_rev proof complete

--- src/ast_instructions.v
+++ src/ast_instructions.v
@@ -17,127 +17,70 @@ Inductive operande :=
 | imm_o : imediate -> operande
 | reg_o : register -> operande.
 
-Scheme Equality for operande.
-
-Inductive tag_op_types :=
-  | ts : list op_type -> tag_op_types.
-
-
-
-  
-Fixpoint op_types_beq (l1 l2 : list op_type) :=
-match l1, l2 with
-        | h1::x1, h2::x2 => andb (op_type_beq h1 h2) (op_types_beq x1 x2)
-        | nil, nil => true
-        | _, _ => false
-end
-.
-
-Fixpoint tag_op_types_beq (t1 t2 : tag_op_types) :=
-  match t1, t2 with
-    ts l1, ts l2 => op_types_beq l1 l2
-  end
-.
-
-Inductive instr {T : Type} :=
-| cinstr : T -> tag_op_types -> instr.
+Inductive instruction {T : Type} :=
+| cinstr : T -> list op_type -> instruction.
 (* pour tester : créer l'ensemble des tags ici *)
 
 Variable tag : Type.
 Variable beq_T : tag -> tag -> bool.
 Variable beq_T_refl : forall (t : tag), beq_T t t = true.
+Variable beq_T_rev : forall (t1 t2 : tag), beq_T t1 t2 = true <-> t1 = t2.
 
-Definition instr_beq (T : Type) (i1 i2 : instr) :=
+Scheme Equality for list.
+
+Definition instruction_beq (T : Type) (i1 i2 : instruction) :=
   match i1,i2 with
-  | cinstr t1 tot1,cinstr t2 tot2 => andb (beq_T t1 t2)  (tag_op_types_beq tot1 tot2) 
+  | cinstr t1 tot1,cinstr t2 tot2 => andb (beq_T t1 t2)  (list_beq op_type op_type_beq tot1 tot2) 
   end.
 
 Search (andb _ _ = true).
 
-Print instr_beq.
+Print instruction_beq.
 Lemma op_type_beq_reflexivity: forall (o: op_type), op_type_beq o o = true.
 Proof.
   destruct o; auto.
 Qed.
 
-Lemma op_types_beq_reflexivity: forall (o: list op_type), op_types_beq o o = true.
+Lemma list_op_type_beq_reflexivity: forall (o: list op_type), list_beq op_type op_type_beq o o = true.
 Proof.
   intro.
   induction o; try auto.
   - simpl. rewrite op_type_beq_reflexivity. rewrite IHo. auto.
 Qed.
 
-Lemma tag_op_types_beq_reflexivity: forall (t: tag_op_types), tag_op_types_beq t t = true.
+Lemma list_op_types_beq_rev : forall (t1 t2 : list op_type), list_beq op_type op_type_beq t1 t2 = true -> t1 = t2.
 Proof.
-  destruct t.
-  simpl.
-  rewrite op_types_beq_reflexivity.
-  reflexivity.
+  induction t1.
+  - induction t2.
+    + auto.
+    + discriminate.
+  - induction t2.
+    + discriminate.
+    + case a, a0.
+      * intro. simpl in H. apply IHt1 in H. subst. reflexivity.
+      * discriminate.
+      * discriminate.
+      * intro. simpl in H. apply IHt1 in H. subst. reflexivity.
 Qed.
 
-Lemma tag_op_types_beq_rev : forall (t1: tag_op_types) (t2: tag_op_types), tag_op_types_beq t1 t2 = true -> t1 = t2.
-Proof.
-  admit.
-Qed.
-
-Lemma instr_beq_reflexivity : forall (t : instr), instr_beq tag t t = true.
+Lemma instruction_beq_reflexivity : forall (t : instruction), instruction_beq tag t t = true.
 Proof.
   intro t.
-  destruct t. simpl. rewrite tag_op_types_beq_reflexivity. rewrite beq_T_refl.
+  destruct t. simpl. rewrite list_op_type_beq_reflexivity. rewrite beq_T_refl.
   auto.
 Qed.
 
-Lemma beq_T_rev: forall (t1 t2 : tag), beq_T t1 t2 = true -> t1=t2.
-Proof.
-  intros.
-  admit.
-Qed.
-
-Lemma instr_rev: forall  (t1 t3 : tag)(t2 t4 : tag_op_types), instr_beq tag (cinstr t1 t2) (cinstr t3 t4)=true -> andb (beq_T t1 t3) (tag_op_types_beq t2 t4)=true.
-Proof.
-  admit.
-Qed.
-
+Definition instruction_rev: forall  (t1 t3 : tag)(t2 t4 : list op_type), instruction_beq tag (cinstr t1 t2) (cinstr t3 t4)=true -> andb (beq_T t1 t3) (list_beq op_type op_type_beq t2 t4)=true.
 
 (*=tag_beq_different *)  
-Lemma instr_beq_different : forall (t1 t2 : instr),
-    instr_beq tag t1 t2 = true -> t1=t2.
+Lemma instruction_beq_different : forall (t1 t2 : instruction),
+    instruction_beq tag t1 t2 = true -> t1=t2.
 Proof.
-  intros. destruct t1. destruct t2. simpl in H. rewrite Bool.andb_true_iff in H. destruct H. apply tag_op_types_beq_rev in H0.
+  intros. destruct t1. destruct t2. simpl in H. rewrite Bool.andb_true_iff in H. destruct H. apply list_op_types_beq_rev in H0.
   rewrite H0. apply beq_T_rev in H.
-  rewrite H. 
-  
+  rewrite H. auto.
 Qed.
   
-    
-
-(* maybe it's not usefull to distinguish the special register than the 
-other because the specification says that you have different numbers for them *)
-
-(* instruction definition *)
-(*=instruction_tern_n *)
-Record instruction_duo :=
-  mk_instr_d { instr_opcode_t_n : tag_duo; 
-             instr_operande1_t : operande ; 
-             instr_operande2_t : operande }.
-(*=End *)
-
-Record instruction_uno :=
-  mk_instr_u { instr_opcode_d : tag_uno; 
-             instr_operande1_d : operande }.
-
-Record instruction_zero :=
-  mk_instr_z { instr_opcode_s : tag_zero }.
-
-
-
-(*=instruction *)
-Inductive instruction :=
-| instr_d : instruction_duo -> instruction
-| instr_u : instruction_uno -> instruction
-| instr_z : instruction_zero -> instruction.
-
-(*=End *)
 (*=binary_instruction  *)
 Definition binary_instruction := list bool.
 (*=End *)
